06eb0861fa4cf1bb8fe043e954965ff9a2d9597a
Wouter Malan <WouterM@gobid.co.za>
2024-06-11 15:16:53 +0200

    chore: Update JWT configuration in appsettings.json

--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e441409f943a925c6564faae675f58449c955c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d93a81233616a153a5dccdb71211d9d7130c62ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
